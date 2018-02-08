960181f273071194f9cae5c3ca57d423ee7ca750
npit <pittarasnikif@gmail.com>
2018-02-08 21:59:53 +0200

    Update timer

--- src/main/java/gr/demokritos/iit/skel/yds/ydsmatcher/Timer.java
+++ src/main/java/gr/demokritos/iit/skel/yds/ydsmatcher/Timer.java
@@ -17,26 +17,42 @@ public class Timer {
         return cal.getTime().toString();
 
     }
-    public static Long toc(String msg){
+
+    public static void peek(String msg){
+        long msec = tocPeek(msg);
+        String tdiff = tostr(msec);
+        System.out.println(String.format("Elapsed for [%s] : %s", msg, tdiff ));
+    }
+
+    public static Long tocPeek(String msg){
         if (! times.containsKey(msg)){
             System.err.println("No tic with header " + msg + "!");
             return 0l;
         }
         Long diff = System.currentTimeMillis() - times.get(msg);
-        times.remove(msg);
         // return milliseconds
         return diff;
 
     }
+
+    public static Long toc(String msg){
+        long diff = tocPeek(msg);
+        times.remove(msg);
+        // return milliseconds
+        return diff;
+    }
+
     public static void tictell(String msg){
 
         System.out.println("Starting timing: " + msg + "  [" + tic(msg) + "]");
     }
-    public static void toctell(String msg){
+
+    public static void tell(String msg){
         long msec = toc(msg);
         String tdiff = tostr(msec);
         System.out.println(String.format("Elapsed for [%s] : %s", msg, tdiff ));
     }
+
     public static String tostr(long msecs){
         if (msecs < 1000) return String.format("%d msecs", msecs);
         long secs = msecs / 1000;
